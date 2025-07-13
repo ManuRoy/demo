08e092f3af2e1e4e5ef2fca926a8b9e948abf1f1
ManuRoy <83333663+ManuRoy@users.noreply.github.com>
2025-07-14 01:49:49 +0530

    Update Security.java

--- demo/Security.java
+++ demo/Security.java
@@ -1,18 +1,1 @@
-package com.example.demo;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public class Security {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-            .authorizeHttpRequests()
-            .anyRequest().permitAll(); // Allow all requests without login
-        return http.build();
-    }
-}
